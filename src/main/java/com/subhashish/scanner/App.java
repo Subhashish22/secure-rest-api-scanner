e9ef093387213a7b4b80866ac9d26864d97b9ea3
Subhashish Dutta <subhashishdutta@Subhashishs-MacBook-Air.local>
2026-07-26 00:40:15 +0530

    Initial Maven project setup

---
+++ src/main/java/com/subhashish/scanner/App.java
@@ -0,0 +1,17 @@
+package com.subhashish.scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("=================================");
+System.out.println(" Secure REST API Scanner");
+System.out.println(" Version 0.1");
+System.out.println(" Developed by Subhashish");
+System.out.println("=================================");;
+    }
+}
