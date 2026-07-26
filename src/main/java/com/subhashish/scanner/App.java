63067f31ad356a2f34987656f9c2b4b48035b1af
Subhashish Dutta <subhashishdutta@Subhashishs-MacBook-Air.local>
2026-07-27 00:54:50 +0530

    Added HTTP request and response handling

--- src/main/java/com/subhashish/scanner/App.java
+++ src/main/java/com/subhashish/scanner/App.java
@@ -1,17 +1,31 @@
 package com.subhashish.scanner;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
         System.out.println("=================================");
-System.out.println(" Secure REST API Scanner");
-System.out.println(" Version 0.1");
-System.out.println(" Developed by Subhashish");
-System.out.println("=================================");;
-    }
+        System.out.println(" Secure REST API Scanner");
+        System.out.println(" Version 0.1");
+        System.out.println(" Developed by Subhashish");
+        System.out.println("=================================");
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status Code: " + response.statusCode());
+        System.out.println(response.body());
+    }  
 }
